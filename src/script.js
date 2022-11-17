12203f9cd320a6ff23a155920d0334f180733267
Glecinilson Silva <glecinilson@gmail.com>
2022-11-16 21:57:53 -0400

    Breaking app on purpose two

--- src/script.js
+++ src/script.js
@@ -6,7 +6,7 @@ const phoneField = document.getElementById('phone')
 document.querySelector('#phone-checkbox')
   .addEventListener('change', function () {
     if (this.checked) {
-      phoneLabelSpan.style.display = 'inlines'
+      phoneLabelSpan.style.display = 'inline'
       phoneField.required = !isPhoneRequired
       isPhoneRequired = !isPhoneRequired
     } else {
